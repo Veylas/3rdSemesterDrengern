334dd96bb15fffdece0a0a5628dcab92c632ccdd
Veylas <manchristoffer@yahoo.dk>
2019-11-22 11:41:46 +0100

    first commit

---
+++ Auktion-bongo-bongo/Auktion/Auktionigen/DataLibrary/BusinessLogic/AuctionProcessor.cs
@@ -0,0 +1,47 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class AuctionProcessor
+    {
+        public static int BidOnAuction(float bid, float currentPrice, int id)
+        {
+            AuctionModel data = new AuctionModel
+            {
+                Bid = bid,
+                Id = id,
+                CurrentPrice = currentPrice * bid
+            };
+            string sql = @"update dbo.Auction set currentPrice = @CurrentPrice
+            where id = @Id";
+            return SqlDataAccess.SaveData(sql, data);
+        }
+            public static int CreateAuction(int status, 
+                   float currentPrice, float maxPrice)
+        {
+            AuctionModel data = new AuctionModel
+            {
+                Status = status,
+                CurrentPrice = currentPrice,
+                MaxPrice = maxPrice
+            };
+            string sql = @"insert into dbo.Auction (status, currentPrice, maxPrice)
+            values (@Status, @CurrentPrice, @MaxPrice);";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+         
+        public static List<AuctionModel> LoadAuctions()
+        {
+            string sql = @"select id, endDate, time, status, currentPrice, maxPrice, currentWinner
+                           from dbo.Auction;";
+            return SqlDataAccess.LoadData<AuctionModel>(sql);
+        }
+    }
+}
---
+++ Auktion-bongo-bongo/Auktion/Auktionigen/TestLogin/Controllers/HomeController.cs
@@ -0,0 +1,148 @@
+﻿using TestLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLibrary;
+using DataLibrary.BusinessLogic;
+
+
+namespace TestLogin.Controllers
+{
+            
+    public class HomeController : Controller
+    {
+        public ActionResult BidOnAuction(int Id = 1, float CurrentPrice=1, float Bid=1)
+        {
+            ViewBag.Message = "Udfyld auktionens informationer her her";
+
+            ViewData["Id"] = Id;
+            ViewData["CurrentPrice"] = CurrentPrice;
+            ViewData["Bid"] = Bid;
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult BidOnAuction(AuctionModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                int recordsCreated = AuctionProcessor.BidOnAuction(
+                    model.Bid,
+                    model.CurrentPrice,
+                    model.Id
+                    );
+                
+                return RedirectToAction("ViewAuction");
+            }
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ViewProduct()
+        {
+            ViewBag.Message = "Auction list";
+
+            var data = ProductProcessor.LoadProduct();
+
+            List<ProductModel> products = new List<ProductModel>();
+            foreach (var row in data)
+            {
+                products.Add(new ProductModel
+                {
+                    Titel = row.Titel,
+                    Description = row.Description,
+                    Catagory = row.Catagory
+                });
+            }
+
+            return View(products);
+        }
+
+
+
+        public ActionResult CreateAuction()
+        {
+            ViewBag.Message = "Udfyld auktionens informationer her her";
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateAuction(AuctionModel model)
+        {
+            if(ModelState.IsValid)
+                
+                {
+                int recordsCreated = AuctionProcessor.CreateAuction(model.Status,
+                    model.CurrentPrice,
+                    model.MaxPrice
+                    );
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult ViewAuction()
+        {
+            ViewBag.Message = "Auction list";
+
+            var data = AuctionProcessor.LoadAuctions();
+
+            List<AuctionModel> auctions = new List<AuctionModel>();
+            foreach (var row in data)
+            {
+                auctions.Add(new AuctionModel
+                {
+                    Id = row.Id,
+                    Status = row.Status,
+                    CurrentPrice = row.CurrentPrice,
+                    MaxPrice = row.MaxPrice,
+                    Bid = row.Bid
+                });
+            }
+
+            return View(auctions);
+        }
+
+
+            public ActionResult CreateProduct()
+        {
+            ViewBag.Message = "Udfyld auktionens informationer her her";
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateProduct(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var recordsCreated = ProductProcessor.CreateProduct(model.Titel,
+                    model.Description,
+                    model.Catagory);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
